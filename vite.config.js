6091e1c850f317c654cfba2b2dd9d678f0e4079f
dhanushkodi192 <dhanushy425@gmail.com>
2026-05-10 23:11:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  preview: {
-    allowedHosts: ['team-task-manager-production-a6507.up.railway.app']
-  }
+  base: './'
 })
